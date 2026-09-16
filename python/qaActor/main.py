320c35be79d5aec2cd1c07684d734e06ac7ab0b2
Wilfred Tyler Gee <wtylergee@gmail.com>
2026-09-15 17:51:12 -1000

    Fix drp2 model reference. - tickets/INSTRM-3013
    * Reason the ingest wasn't working after switch.

--- python/qaActor/main.py
+++ python/qaActor/main.py
@@ -22,14 +22,14 @@ class QaActor(ICC):
         self.attachAllControllers()
 
         # Attach a model of an external actor so we can listen to its properties.
-        _models = ("drp",)
+        _models = ("drp2",)
         self.drp = Drp(
             actor=self, processing_queue=self.controllers["qa"].processing_queue, logger=self.logger
         )
         self.addModels(_models)
 
         # Add a listener on the Drp model for when reduceExposure task is complete.
-        self.models["drp"].keyVarDict["reduceExposureStatus"].addCallback(
+        self.models["drp2"].keyVarDict["reduceExposureStatus"].addCallback(
             self.drp.check_reduced_exposure_status, callNow=False
         )
 
